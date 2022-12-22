6fb120ca9658e333749eaf99666abafdc6179cc1
jss <wltkqdl@gmail.com>
2022-12-22 19:05:34 +0900

    aop

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,15 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // JPA가 관리하는 Entity
 public class Member {
 
+    @Id // pk
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // ID는 DB가 정함
     private Long id;    // 시스템이 정함
     private String name;    // 회원이 적음
 
